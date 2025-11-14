7b156f8ba2592cb8693599bd87d5670e4adba977
Elondo Mbonze <elondo.mbonze@bighand.com>
2025-11-14 01:10:36 +0000

    Linting issue

--- app.go
+++ app.go
@@ -457,7 +457,6 @@ func configureI18n(cfg *Config) {
 }
 
 func configureJSONP(cfg *Config) {
-
 	if cfg != nil {
 		if cfg.JSONPCallbackParamName != "" {
 			matched := jsonpCallbackNamePattern.MatchString(cfg.JSONPCallbackParamName)
